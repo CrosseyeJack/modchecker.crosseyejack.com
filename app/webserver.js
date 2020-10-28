e9176222095850600fd247bfd2718200892b4c09
Crosseye Jack <dan@crosseyejack.com>
2020-10-28 17:42:30 +0000

    Removed some code duplication and more
    Moved the call to TMI into its own promise so it can be sharded between the two api calls
    
    added the capture of "no mods" in a channel.
    
    added the channel owner to the list of mods if they are not a mod already.
    
    sorted the mod list.
    
    drank a beer.

--- app/webserver.js
+++ app/webserver.js
@@ -5,110 +5,14 @@ const app = express();
 
 const twitchChat = require("./twitchWebsocket");
 
+const twitchUsernameRegex = /^([a-z0-9_]{4,25})$/i;
+
 app.use(express.json());
 app.get("/:channel", (req, res) => handleChannelMods(req, res)); // return all the mods of a channel.
 app.get("/:channel/:user", (req, res) => handleChannelModsCheck(req, res)); // return a simple yes/no of :user is a mod of :channel
 
-// FIXME Prob gonna rewrite this to cut down on the reused code.
-const handleChannelModsCheck = (req, res) => {
-  if ("channel" in req.params && "user" in req.params) {
-    const channelName = req.params.channel.toLowerCase(),
-      user = req.params.user.toLowerCase(),
-      ws = twitchChat.ws();
-
-    // Simple username validation check
-    if (
-      !channelName.match(/^([a-z0-9_]{4,25})$/i) ||
-      !user.match(/^([a-z0-9_]{4,25})$/i)
-    ) {
-      res.writeHead(500, {
-        "Content-Type": "application/json",
-        "Cache-Control": "private, max-age=0, no-cache",
-      });
-      res.end(
-        JSON.stringify({ error: "params", errormsg: "Invalid parameters." })
-      );
-      return;
-    }
-
-    if (ws.readyState !== WebSocket.OPEN) {
-      res.writeHead(503, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          error: "twitch",
-          errormsg: "Unable to communicate with Twitch. Try again later.",
-        })
-      );
-      return;
-    }
-
-    let timeoutObj, resolveObj;
-
-    const messageProcessor = (data) => {
-      data.split(/\r?\n/).forEach((line) => {
-        if (line.length === 0) return;
-        if (
-          line.startsWith(
-            `:tmi.twitch.tv NOTICE #${channelName} :The moderators of this channel are:`
-          )
-        ) {
-          resolveObj(line);
-          return;
-        }
-      });
-    };
-    new Promise((resolve, reject) => {
-      resolveObj = resolve;
-      timeoutObj = setTimeout(() => {
-        reject();
-      }, 2500);
-
-      ws.on("message", messageProcessor);
-      twitchChat.sendMessage(`PRIVMSG #${channelName} :.mods`);
-    })
-      .then((data) => {
-        clearTimeout(timeoutObj);
-        ws.removeEventListener("message", messageProcessor);
-
-        const listOfMods = data
-          .substring(
-            `:tmi.twitch.tv NOTICE #${channelName} :The moderators of this channel are: `
-              .length
-          )
-          .split(", ");
-        if (listOfMods.includes(user)) {
-          res.writeHead(200, {
-            "Content-Type": "application/json",
-            "Cache-Control":
-              "public, min-fresh=60, max-age=3600, must-revalidate",
-          });
-          res.end(JSON.stringify(listOfMods));
-          return;
-        } else {
-          res.writeHead(404, {
-            "Content-Type": "application/json",
-            "Cache-Control": "private, max-age=0, no-cache",
-          });
-          res.end(JSON.stringify(listOfMods));
-          return;
-        }
-      })
-      .catch((error) => {
-        ws.removeEventListener("message", messageProcessor);
-        res.writeHead(503, {
-          "Content-Type": "application/json",
-          "Cache-Control": "private, max-age=0, no-cache",
-        });
-        res.end(
-          JSON.stringify({
-            error: "twitch",
-            errormsg:
-              "Unable to communicate with Twitch. Try again later. Please double check the channel name, if the channel doesn't exist twitch won't tell me without another API call and I'm a lazy bum.",
-          })
-        );
-        return;
-      });
-  } else {
+const handleChannelMods = (req, res) => {
+  if (!("channel" in req.params)) {
     res.writeHead(500, {
       "Content-Type": "application/json",
       "Cache-Control": "private, max-age=0, no-cache",
@@ -118,27 +22,35 @@ const handleChannelModsCheck = (req, res) => {
     );
     return;
   }
-};
 
-const handleChannelMods = (req, res) => {
-  if ("channel" in req.params) {
-    let channelName = req.params.channel;
+  let channelName = req.params.channel;
 
-    // Simple username validation check
-    if (!channelName.match(/^([a-z0-9_]{4,25})$/i)) {
+  // Simple username validation check
+  if (!channelName.match(twitchUsernameRegex)) {
+    res.writeHead(500, {
+      "Content-Type": "application/json",
+      "Cache-Control": "private, max-age=0, no-cache",
+    });
+    res.end(
+      JSON.stringify({ error: "params", errormsg: "Invalid parameters." })
+    );
+    return;
+  }
+
+  getMods(channelName)
+    .then((modList) => {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+        "Cache-Control": "public, min-fresh=60, max-age=3600, must-revalidate",
+      });
+      res.end(JSON.stringify(modList));
+      return;
+    })
+    .catch((error) => {
       res.writeHead(500, {
         "Content-Type": "application/json",
         "Cache-Control": "private, max-age=0, no-cache",
       });
-      res.end(
-        JSON.stringify({ error: "params", errormsg: "Invalid parameters." })
-      );
-      return;
-    }
-
-    const ws = twitchChat.ws();
-    if (ws.readyState !== WebSocket.OPEN) {
-      res.writeHead(503, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           error: "twitch",
@@ -146,77 +58,126 @@ const handleChannelMods = (req, res) => {
         })
       );
       return;
-    }
+    });
+};
 
-    let timeoutObj, resolveObj;
+// FIXME Prob gonna rewrite this to cut down on the reused code.
+const handleChannelModsCheck = (req, res) => {
+  if (!("channel" in req.params) || !("user" in req.params)) {
+    res.writeHead(500, {
+      "Content-Type": "application/json",
+      "Cache-Control": "private, max-age=0, no-cache",
+    });
+    res.end(
+      JSON.stringify({ error: "params", errormsg: "Invalid parameters." })
+    );
+    return;
+  }
 
-    const messageProcessor = (data) => {
-      data.split(/\r?\n/).forEach((line) => {
-        if (line.length === 0) return;
-        if (
-          line.startsWith(
-            `:tmi.twitch.tv NOTICE #${channelName} :The moderators of this channel are:`
-          )
-        ) {
-          resolveObj(line);
-          return;
-        }
-      });
-    };
-    new Promise((resolve, reject) => {
-      resolveObj = resolve;
-      timeoutObj = setTimeout(() => {
-        reject();
-      }, 2500);
+  const channelName = req.params.channel.toLowerCase(),
+    user = req.params.user.toLowerCase();
 
-      ws.on("message", messageProcessor);
-      twitchChat.sendMessage(`PRIVMSG #${channelName} :.mods`);
-    })
-      .then((data) => {
-        clearTimeout(timeoutObj);
-        ws.removeEventListener("message", messageProcessor);
+  // Simple username validation check
+  if (
+    !channelName.match(twitchUsernameRegex) ||
+    !user.match(twitchUsernameRegex)
+  ) {
+    res.writeHead(500, {
+      "Content-Type": "application/json",
+      "Cache-Control": "private, max-age=0, no-cache",
+    });
+    res.end(
+      JSON.stringify({ error: "params", errormsg: "Invalid parameters." })
+    );
+    return;
+  }
+
+  getMods(channelName)
+    .then((modList) => {
+      if (modList.includes(user))
         res.writeHead(200, {
           "Content-Type": "application/json",
           "Cache-Control":
             "public, min-fresh=60, max-age=3600, must-revalidate",
         });
-        res.end(
-          JSON.stringify(
-            data
-              .substring(
-                `:tmi.twitch.tv NOTICE #${channelName} :The moderators of this channel are: `
-                  .length
-              )
-              .split(", ")
-          )
-        );
-        return;
-      })
-      .catch((error) => {
-        ws.removeEventListener("message", messageProcessor);
-        res.writeHead(503, {
+      else
+        res.writeHead(404, {
           "Content-Type": "application/json",
-          "Cache-Control": "private, max-age=0, no-cache",
+          "Cache-Control":
+            "public, min-fresh=60, max-age=3600, must-revalidate",
         });
-        res.end(
-          JSON.stringify({
-            error: "twitch",
-            errormsg:
-              "Unable to communicate with Twitch. Try again later. Please double check the channel name, if the channel doesn't exist twitch won't tell me without another API call and I'm a lazy bum.",
-          })
-        );
-        return;
+      res.end(JSON.stringify(modList));
+      return;
+    })
+    .catch((error) => {
+      res.writeHead(500, {
+        "Content-Type": "application/json",
+        "Cache-Control": "private, max-age=0, no-cache",
       });
-  } else {
-    res.writeHead(500, {
-      "Content-Type": "application/json",
-      "Cache-Control": "private, max-age=0, no-cache",
+      res.end(
+        JSON.stringify({
+          error: "twitch",
+          errormsg: "Unable to communicate with Twitch. Try again later.",
+        })
+      );
+      return;
     });
-    res.end(
-      JSON.stringify({ error: "params", errormsg: "Invalid parameters." })
-    );
-    return;
-  }
+};
+
+const getMods = (channelName) => {
+  return new Promise((resolve, reject) => {
+    const ws = twitchChat.ws(),
+      timeoutObj = setTimeout(() => {
+        ws.removeEventListener("message", messageProcessor);
+        reject();
+      }, 2500);
+
+    const messageProcessor = (data) => {
+      data.split(/\r?\n/).forEach((line) => {
+        if (line.length === 0) return;
+        // TODO Add a check for "there are no moderators"
+        if (
+          line.startsWith(
+            `:tmi.twitch.tv NOTICE #${channelName} :The moderators of this channel are:`
+          )
+        ) {
+          ws.removeEventListener("message", messageProcessor);
+          clearInterval(timeoutObj);
+          const modList = data
+            .substring(
+              `:tmi.twitch.tv NOTICE #${channelName} :The moderators of this channel are: `
+                .length
+            )
+            .trim()
+            .split(", ");
+          // if the channel owner is not in the mod list - add them, they own the channel afterall.
+          if (!modList.includes(channelName)) modList.push(channelName);
+          resolve(modList.sort());
+          return;
+        } else if (
+          line.startsWith(
+            `:tmi.twitch.tv NOTICE #${channelName} :There are no moderators of this channel.`
+          )
+        ) {
+          ws.removeEventListener("message", messageProcessor);
+          clearInterval(timeoutObj);
+          resolve([]); // No mods so just return an empty array.
+          return;
+        }
+      });
+    };
+
+    // Check if the websocket connection is actually open.
+    if (ws.readyState !== WebSocket.OPEN) {
+      clearInterval(timeoutObj);
+      reject();
+      return;
+    }
+
+    // bind the message listener and send the mod list request
+    ws.on("message", messageProcessor);
+    twitchChat.sendMessage(`PRIVMSG #${channelName} :.mods`);
+  });
 };
 
 module.exports = {
